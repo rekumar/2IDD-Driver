f0a7d255f9b83195608d34000cd1fe16d76a7024
grace227 <37392904+grace227@users.noreply.github.com>
2022-03-07 11:10:42 -0600

    Update scan2idd.py

--- s2driver/scan2idd.py
+++ s2driver/scan2idd.py
@@ -7,7 +7,7 @@ import os
 import time
 import sys
 import numpy as np
-from imgProcessing import getROIcoordinate_data, getElmMap
+# from imgProcessing import getROIcoordinate_data, getElmMap
 
 
 def parmLabelToPVdict(scanType):
@@ -57,50 +57,50 @@ def scanFinish(pvComm):
     pvComm.closeShutter()
 
 
-def fileReady(coarse_sc, fdir, tlim=30):
-    fpath = os.path.join(fdir, 'img.dat/%s.h5' % (coarse_sc))
-    if os.path.exists(fpath):
-        fmtime = os.path.getmtime(fpath)
-        tdiff = time.time() - fmtime
-        if tdiff > tlim:
-            return 1
-        else:
-            sys.stdout.write('Waiting for coarse scan file %s.h5 to be ready,'
-                             ' file modified time: %d, time difference: %d \n'
-                             % (coarse_sc, fmtime, tdiff))
-            return 0
-    else:
-        sys.stdout.write('File %s not exisit\n' % fpath)
-        return 0
+# def fileReady(coarse_sc, fdir, tlim=30):
+#     fpath = os.path.join(fdir, 'img.dat/%s.h5' % (coarse_sc))
+#     if os.path.exists(fpath):
+#         fmtime = os.path.getmtime(fpath)
+#         tdiff = time.time() - fmtime
+#         if tdiff > tlim:
+#             return 1
+#         else:
+#             sys.stdout.write('Waiting for coarse scan file %s.h5 to be ready,'
+#                              ' file modified time: %d, time difference: %d \n'
+#                              % (coarse_sc, fmtime, tdiff))
+#             return 0
+#     else:
+#         sys.stdout.write('File %s not exisit\n' % fpath)
+#         return 0
 
 
-def imgProgFolderCheck(fdir):
-    img_path = os.path.join(fdir, 'imgProg')
-    if not os.path.exists(img_path):
-        os.makedirs(img_path)
-    return img_path
+# def imgProgFolderCheck(fdir):
+#     img_path = os.path.join(fdir, 'imgProg')
+#     if not os.path.exists(img_path):
+#         os.makedirs(img_path)
+#     return img_path
 
 
-def getCoordinate(pvComm, coarse_sc, scandic, n_std=2):
-    fready = fileReady(coarse_sc, pvComm.userdir)
-    coarse_h5path = os.path.join(pvComm.userdir, 'img.dat/%s.h5' % (coarse_sc))
-    if fready:
-        imgfolder = imgProgFolderCheck(pvComm.userdir)
-        imgpath = os.path.join(imgfolder, 'bbox_%s.png' % (coarse_sc))
-        print(imgpath)
-        elmmap = getElmMap(coarse_h5path, scandic['elm'])
+# def getCoordinate(pvComm, coarse_sc, scandic, n_std=2):
+#     fready = fileReady(coarse_sc, pvComm.userdir)
+#     coarse_h5path = os.path.join(pvComm.userdir, 'img.dat/%s.h5' % (coarse_sc))
+#     if fready:
+#         imgfolder = imgProgFolderCheck(pvComm.userdir)
+#         imgpath = os.path.join(imgfolder, 'bbox_%s.png' % (coarse_sc))
+#         print(imgpath)
+#         elmmap = getElmMap(coarse_h5path, scandic['elm'])
 
-        mask = np.ones(elmmap[0].shape)
-        if scandic['use_mask']:
-            maskmap = getElmMap(coarse_h5path, scandic['mask_elm'])
-            mask = maskmap < (np.mean(maskmap) + n_std *
-                              np.std(maskmap.ravel()))
-        m = elmmap[0] * mask
+#         mask = np.ones(elmmap[0].shape)
+#         if scandic['use_mask']:
+#             maskmap = getElmMap(coarse_h5path, scandic['mask_elm'])
+#             mask = maskmap < (np.mean(maskmap) + n_std *
+#                               np.std(maskmap.ravel()))
+#         m = elmmap[0] * mask
 
-        x, y, w, h = getROIcoordinate_data(m, elmmap[1], elmmap[2],
-                                           n_cluster=scandic['n_clusters'],
-                                           sel_cluster=scandic['sel_cluster'],
-                                           figpath=imgpath)
-        return np.round(x, 2), np.round(y, 2)
-    else:
-        return None
+#         x, y, w, h = getROIcoordinate_data(m, elmmap[1], elmmap[2],
+#                                            n_cluster=scandic['n_clusters'],
+#                                            sel_cluster=scandic['sel_cluster'],
+#                                            figpath=imgpath)
+#         return np.round(x, 2), np.round(y, 2)
+#     else:
+#         return None
