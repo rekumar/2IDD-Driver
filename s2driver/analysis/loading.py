d2eb3b0c455be715f6871474064f61096cd6f84a
Rishi Kumar <rishi42@gmail.com>
2022-03-13 15:23:30 -0700

    xeol code first draft integration from deniz

---
+++ s2driver/analysis/loading.py
@@ -0,0 +1,118 @@
+import h5py
+import numpy as np
+
+
+def load_h5(fpath, clip_flyscan=True, xbic_on_dsic=False, quant_scaler="us_ic"):
+    """
+    Loads a MAPS-generated .h5 file (raw + fitted data from 2IDD, fitted from 26IDC)
+
+    fpath: path to file
+    clip_flyscan: boolean, removes last two columns of map (which are corrupted when running flyscans)
+    xbic_on_dsic: boolean. if True, considers the downstream ion chamber to be connected in an XBIC setup, adds to output['maps']['XBIC']
+    quant_scaler: one of ['sr_current', 'us_ic', 'ds_ic']. quantification is normalized to some metric of beam power deposited in sample. typically we use upstream ion chamber
+
+    returns a dictionary with scan info and maps.
+    """
+
+    quant_scaler_key = {
+        "sr_current": 0,  # storage ring current
+        "us_ic": 1,  # upstream ion chamber (incident flux)
+        "ds_ic": 2,  # downstream ion chamber (transmitted flux - make sure this sensor was not used for XBIC!)
+    }
+    if quant_scaler not in quant_scaler_key:
+        raise ValueError(
+            f"Quantification normalization must be by either sr_current, us_ic, or ds_ic! user provided {quant_scaler}."
+        )
+
+    output = {"filepath": fpath}
+    if clip_flyscan:
+        xmask = slice(0, -2)  # last two columns are garbage from flyscan, omit here
+    else:
+        xmask = slice(0, None)  # no clipping
+
+    with h5py.File(fpath, "r") as dat:
+        output["x"] = dat["MAPS"]["x_axis"][()]
+        output["y"] = dat["MAPS"]["y_axis"][()]
+        output["spectra"] = np.moveaxis(dat["MAPS"]["mca_arr"][()], 0, 2)[
+            :, xmask
+        ]  # y by x by energy, full XRF spectra
+        output["energy"] = dat["MAPS"]["energy"][
+            : output["spectra"].shape[-1]
+        ]  # specmap only has 2000 bins sometimes, match energy to that
+        output["intspectra"] = dat["MAPS"]["int_spec"][
+            : output["spectra"].shape[-1]
+        ]  # integrated spectra
+        output["extent"] = [
+            output["x"][0],
+            output["x"][-1],
+            output["y"][0],
+            output["y"][-1],
+        ]
+
+        scaler_names = dat["MAPS"]["scaler_names"][()].astype("U13").tolist()
+        quant_scaler_values = np.array(
+            dat["MAPS"]["scalers"][scaler_names.index(quant_scaler)]
+        )[:, xmask]
+        fillval = np.nanmean(quant_scaler_values[quant_scaler_values > 0])
+        quant_scaler_values = np.where(
+            quant_scaler_values == 0.0,
+            fillval,
+            quant_scaler_values,
+        )  # change all values that are 0.0 to mean, avoid divide by 0
+        if "/MAPS/XRF_fits" in dat:
+            xrf = []
+            raw = dat["MAPS"]["XRF_fits"][
+                :, :, xmask
+            ]  # xrf elemental maps, elements by y by x
+            quant = np.moveaxis(
+                dat["MAPS"]["XRF_fits_quant"][()], 2, 0
+            )  # quantification factors from MAPS fitting, elements by factors
+            for x, q in zip(raw, quant):
+                x = np.divide(
+                    x, quant_scaler_values
+                )  # normalize to quantification scaler
+                quantfactor = q[quant_scaler_key[quant_scaler]]
+                xrf.append(
+                    x / quantfactor / 4
+                )  # factor of 4 came from discussion w/ Arthur Glowacki @ APS, and brings this value in agreement with MAPS displayed value. not sure why it is necessary though...
+                # update 20221228: this factor changes from run to run, but is always a round number (have seen 1, 4, and 10). I expect it could be related to usic amplifier settings or similar, but cant find a related value in the h5 file REK
+            output["fitted"] = True
+        else:
+            xrf = dat["MAPS"]["XRF_roi"][:, :, xmask]
+            output["fitted"] = False
+
+        allchan = dat["MAPS"]["channel_names"][()].astype("U13").tolist()
+        if xbic_on_dsic:
+            xrf.append(
+                dat["MAPS"]["scalers"][scaler_names.index("ds_ic")][:, xmask]
+            )  # append DSIC to channels, used for xbic
+            allchan.append("XBIC")  # label for DSIC
+    output["maps"] = {}
+    for channel, xrf_ in zip(allchan, xrf):
+        output["maps"][channel] = np.array(xrf_)
+    return output
+
+
+def load_xeol(fpath):
+    output = {}
+    with h5py.File(fpath, "r") as dat:
+        wl = dat["wavelength"][()]
+        bg = dat["background"][()]
+        counts_raw = dat["spectra"][()]
+        dwelltime = dat["dwelltime"][()]
+
+    cps = (counts_raw - bg) / dwelltime  # counts per second
+    peak_cps_per_pixel = cps.max(axis=2)
+    peak_wl_per_pixel = wl[cps.argmax(axis=2)]
+
+    output = {
+        "wavelength": wl,
+        "background_counts": bg,
+        "raw_counts": counts_raw,
+        "dwelltime": dwelltime,
+        "cps": cps,
+        "peak_cps_per_pixel": peak_cps_per_pixel,
+        "peak_wl_per_pixel": peak_wl_per_pixel,
+    }
+
+    return output
---
+++ s2driver/xeol/spectrometer.py
@@ -0,0 +1,205 @@
+import numpy as np
+import h5py as h5
+import h5py
+import os
+import epics
+import epics.devices
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import time
+
+import stellarnet_driver3 as sn  # usb driver
+
+# you cant import stellarnet unless you are in the right directory regardless of env
+
+
+class Stellarnet:
+    """Object to interface with Stellarnet spectrometer"""
+
+    def __init__(self, address="ttyS0"):
+        self.id, self.__wl = sn.array_get_spec(address)
+        self.SETTING_DELAY = (
+            0.2  # seconds between changing a setting and having it take effect
+        )
+
+        print("Connected to spectrometer")
+        self.dwelltime = self._hdr_times[0]  # ms
+        self.numscans = 1  # one scan per spectrum
+        self.smooth = 0  # smoothing factor, units unclear
+
+        self.__baseline_dark = {}
+        self.__baseline_light = {}
+
+    ##
+    # function to convert dict to .h5 from
+    # https://codereview.stackexchange.com/questions/120802/recursively-save-python-dictionaries-to-hdf5-files-using-h5py/121308
+
+    def save_dict_to_hdf5(self, dic, filename):
+        """
+        ....
+        """
+        with h5py.File(filename, "w") as h5file:
+            self.recursively_save_dict_contents_to_group(h5file, "/", dic)
+
+    def recursively_save_dict_contents_to_group(self, h5file, path, dic):
+        """
+        ....
+        """
+        for key, item in dic.items():
+            if isinstance(item, (np.ndarray, np.int64, np.float64, str, bytes)):
+                h5file[path + key] = item
+            elif isinstance(item, dict):
+                self.recursively_save_dict_contents_to_group(
+                    h5file, path + key + "/", item
+                )
+            else:
+                raise ValueError("Cannot save %s type" % type(item))
+
+    def load_dict_from_hdf5(self, filename):
+        """
+        ....
+        """
+        with h5py.File(filename, "r") as h5file:
+            return self.recursively_load_dict_contents_from_group(h5file, "/")
+
+    def recursively_load_dict_contents_from_group(self, h5file, path):
+        """
+        ....
+        """
+        ans = {}
+        for key, item in h5file[path].items():
+            if isinstance(item, h5py._hl.dataset.Dataset):
+                ans[key] = item.value
+            elif isinstance(item, h5py._hl.group.Group):
+                ans[key] = self.recursively_load_dict_contents_from_group(
+                    h5file, path + key + "/"
+                )
+        return ans
+
+    def open_h5(self, filename):
+        out = {}
+        with h5py.File(filename, "r") as dat:
+            out["wavelength"] = dat["wavelength"][:]
+            out["time_data"] = dat["time_data"][:]
+
+        return out
+
+    @property
+    def dwelltime(self):
+        return self.__integrationtime
+
+    @dwelltime.setter
+    def dwelltime(self, t):
+        self.id["device"].set_config(int_time=t)
+        time.sleep(self.SETTING_DELAY)
+        self.__integrationtime = t
+
+    @property
+    def numscans(self):
+        return self.__numscans
+
+    @numscans.setter
+    def numscans(self, n):
+        self.id["device"].set_config(scans_to_avg=n)
+        time.sleep(self.SETTING_DELAY)
+        self.__numscans = n
+
+    @property
+    def smooth(self):
+        return self.__smooth
+
+    @smooth.setter
+    def smooth(self, n):
+        if n not in [0, 1, 2, 3, 4]:
+            raise ValueError("Smoothing factor must be 0, 1, 2, 3, or 4")
+        self.id["device"].set_config(x_smooth=n)
+        time.sleep(self.SETTING_DELAY)
+        self.__smooth = n
+
+    def take_light_baseline(self, skip_repeats=False):
+        """takes an illuminated baseline at each integration time from HDR timings"""
+        numscans0 = self.numscans
+        self.numscans = 3
+        for t in self._hdr_times:
+            if skip_repeats and t in self.__baseline_light:
+                continue  # already taken
+            self.dwelltime = t
+            wl, cts = self._capture_raw()
+            self.__baseline_light[t] = cts
+        self.numscans = numscans0
+
+    def take_dark_baseline(self, skip_repeats=False):
+        """takes an dark baseline at each integration time from HDR timings"""
+        numscans0 = self.numscans
+        self.numscans = 3
+        for t in self._hdr_times:
+            if skip_repeats and t in self.__baseline_dark:
+                continue  # already taken
+            self.dwelltime = t
+            wl, cts = self._capture_raw()
+            self.__baseline_dark[t] = cts
+        self.numscans = numscans0
+
+    def __is_dark_baseline_taken(self, dwelltime=None):
+        """Check whether a baseline has been taken at the current integration time
+        Raises:
+            ValueError: Dark baseline has not been taken at the current integration time
+        Returns True if dark baseline has been taken at the current integration time
+        """
+        if dwelltime is None:
+            dwelltime = self.dwelltime
+        if dwelltime not in self.__baseline_dark:
+            raise ValueError(
+                f"Dark baseline not taken for current integration time ({self.dwelltime} ms). Taken for {list(self.__baseline_dark.keys())}"
+            )
+
+        return True
+
+    def __is_light_baseline_taken(self, dwelltime=None):
+        """Check whether a baseline has been taken at the current integration time
+        Raises:
+            ValueError: Light baseline has not been taken at the current integration time
+        Returns True if light baseline has been taken at the current integration time
+        """
+        if dwelltime is None:
+            dwelltime = self.dwelltime
+        if dwelltime not in self.__baseline_light:
+            raise ValueError(
+                f"Illuminated baseline not taken for current integration time ({self.dwelltime}). Taken for {list(self.__baseline_light.keys())}"
+            )
+
+        return True
+
+    # first taking a background before scanning starts
+    def capture_raw(self):
+        """
+        captures a spectrum from the usb spectrometer
+        returns raw wavelength + counts read from spectrometer
+        """
+        t0 = time.time()
+        spectrum = sn.array_spectrum(self.id, self.__wl)  # actual function
+        # spectrum = np.random.rand(2048,2)
+        t1 = time.time()
+
+        # spectrum.shape
+        # spectrum[:, 1] /= self.integrationtime / 1000  # convert to counts per second
+        wl, cts = (
+            spectrum[:, 0].round(2),
+            spectrum[:, 1],
+        )  # wavelength bins are reported as way more precise than they actually are for our slit width
+        tot_time = t1 - t0
+        return wl, cts, tot_time
+
+
+"""
+code to run:
+
+from XEOL_driver_v0 import Spectrometer
+s = Spectrometer()
+s.dwelltime = 500 #in ms
+s.XEOL_start()
+
+test = open_h5('/Volumes/GoogleDrive/My Drive/Inorganic_PSK/s2_analysis/scan100_XEOL.h5')
+test
+"""
