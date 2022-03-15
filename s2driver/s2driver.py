7c6531aa5dbc6d82245ff2b6c610c03219920b3e
Rishi Kumar <rishi42@gmail.com>
2022-03-14 17:19:37 -0700

    only load xeol_controller in driving.py

--- s2driver/s2driver.py
+++ s2driver/s2driver.py
@@ -1,5 +1,4 @@
 from s2driver.closedloop.bridge import S2Server
-from s2driver.xeol.xeol import XEOLController
 from s2driver.driving import *  # all driving commands
 from s2driver.logging import initialize_logbook
 
